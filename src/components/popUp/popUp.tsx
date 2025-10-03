d8265f4e3e9a3b233a45a201729d663a584fa06c
LalitKJ <lalit.k.jangir@gmail.com>
2025-10-03 21:33:26 +0530

    Made Hamburger menu for small devices

--- src/components/popUp/popUp.tsx
+++ src/components/popUp/popUp.tsx
@@ -13,7 +13,8 @@ export default function PopUp({ open, className, onClose, children }: PopupProps
     return (
         <section
             id="custom-popup-backdrop"
-            className={`adjust-top fixed inset-0 backdrop-blur-md bg-opacity-50 flex items-center justify-center z-50 ${className}`}
+            style={{ display: open ? 'block' : 'none' }}
+            className={`fixed inset-0 backdrop-blur-md bg-opacity-50 flex items-center justify-center z-50 ${className}`}
             onClick={onClose} // Close on backdrop click
         >
             {children}
