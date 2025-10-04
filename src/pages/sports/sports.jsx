921cb9c8eba7e8067368adbcdf5a547effdc6add
LalitKJ <lalit.k.jangir@gmail.com>
2025-10-05 01:53:16 +0530

    made new apis for sports page

--- src/pages/sports/sports.jsx
+++ src/pages/sports/sports.jsx
@@ -1,80 +1,98 @@
 import React, { useState } from "react";
 import SportsSection from "../../components/SportsSection";
 import TableComponent from "../../components/table";
-import '../../App.css';
-import { Dialog, DialogTitle, DialogContent, Divider, IconButton } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
+import "../../App.css";
+import {
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	Divider,
+	IconButton,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import getSportsData from "./utils/sports";
 export default function Sports() {
-  const [isPopupOpen, setPopupOpen] = useState(false);
-  const [selectedSportData, setSelectedSportData] = useState(null);
-  const [selectedSportName, setSelectedSportName] = useState("");
+	const [isPopupOpen, setPopupOpen] = useState(false);
+	const [selectedSportData, setSelectedSportData] = useState(null);
+	const [selectedSportName, setSelectedSportName] = useState("");
 
-  // --- Click Handlers ---
-  
-  const handleSportClick = async (sport) => {
-    try {
-      const module = await import(`../../data/${sport.id}.js`);
-      setSelectedSportData(module.default);
-      setSelectedSportName(sport.name);
-      setPopupOpen(true);
-    } catch (error) {
-      alert(`Could not find data for ${sport.name}.`);
-    }
-  };
+	/////////////////////////////
+	const data = getSportsData();
+	console.log(data);
+	/////////////////////////////
 
-  const handleClosePopup = () => {
-    setPopupOpen(false);
-  };
+	// --- Click Handlers ---
 
+	const handleSportClick = async (sport) => {
+		try {
+			const module = await import(`../../data/${sport.id}.js`);
+			setSelectedSportData(module.default);
+			setSelectedSportName(sport.name);
+			setPopupOpen(true);
+		} catch (error) {
+			alert(`Could not find data for ${sport.name}.`);
+		}
+	};
 
-  return (
-    <div className="sports-container">
-      <h1 style={{ textAlign: "center", fontFamily: 'Inter, sans-serif', fontSize: '3rem', fontWeight: '700', color: '#3A1616' }}>
-        SPORTS
-      </h1>
-      <SportsSection handleSportClick={handleSportClick} />
-      <Dialog
-        open={isPopupOpen}
-        onClose={handleClosePopup}
-        fullWidth={true}
-        maxWidth="sm"
-        sx={{ backdropFilter: 'blur(2px)', m: 0 }}
-        PaperProps={{
-          sx: (theme) => ({
+	const handleClosePopup = () => {
+		setPopupOpen(false);
+	};
 
-            borderRadius: '40px',
-            [theme.breakpoints.down('sm')]: {
-              margin: 0,
-              width: '96%',
-            },
-          }),
-        }}
-      >
-        <IconButton
-          aria-label="close"
-          onClick={handleClosePopup}
-          sx={{
-            position: 'absolute',
-            right: 12,
-            top: 12,
-            color: "#946F2C",
-            zIndex: 2
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <div className="popup-container">
-          <DialogTitle >
-            {selectedSportName} Table
-          </DialogTitle>
-          <Divider sx={{ borderColor: '#946F2C', borderBottomWidth: 2, margin: '0 0 10px 0' }} />
+	return (
+		<div className='sports-container'>
+			<h1
+				style={{
+					textAlign: "center",
+					fontFamily: "Inter, sans-serif",
+					fontSize: "3rem",
+					fontWeight: "700",
+					color: "#3A1616",
+				}}>
+				SPORTS
+			</h1>
+			<SportsSection handleSportClick={handleSportClick} />
+			<Dialog
+				open={isPopupOpen}
+				onClose={handleClosePopup}
+				fullWidth={true}
+				maxWidth='sm'
+				sx={{ backdropFilter: "blur(2px)", m: 0 }}
+				PaperProps={{
+					sx: (theme) => ({
+						borderRadius: "40px",
+						[theme.breakpoints.down("sm")]: {
+							margin: 0,
+							width: "96%",
+						},
+					}),
+				}}>
+				<IconButton
+					aria-label='close'
+					onClick={handleClosePopup}
+					sx={{
+						position: "absolute",
+						right: 12,
+						top: 12,
+						color: "#946F2C",
+						zIndex: 2,
+					}}>
+					<CloseIcon />
+				</IconButton>
+				<div className='popup-container'>
+					<DialogTitle>{selectedSportName} Table</DialogTitle>
+					<Divider
+						sx={{
+							borderColor: "#946F2C",
+							borderBottomWidth: 2,
+							margin: "0 0 10px 0",
+						}}
+					/>
 
-          <DialogContent >
-            {<TableComponent teams={selectedSportData} />}
-          </DialogContent>
-
-        </div>
-      </Dialog>
-    </div>
-  );
+					<DialogContent>
+						{<TableComponent teams={selectedSportData} />}
+					</DialogContent>
+				</div>
+			</Dialog>
+		</div>
+	);
 }
