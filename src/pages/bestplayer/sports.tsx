998cce208b08b05a234353f7829f64ca9a957b83
deepak108-sudo <deepakkr977140@gmail.com>
2025-10-06 04:16:16 +0530

    there was the problem from previous merge, tried to compensate this, Problem was due to navbar.scss

--- src/pages/bestplayer/sports.tsx
+++ src/pages/bestplayer/sports.tsx
@@ -13,7 +13,9 @@ export default function BestBySports() {
       {/** Background */}
       <div className="relative min-h-screen bg-gradient-to-tr from-[#fcf0fd] to-[#FFFFF0]">
         <div className="content">
-          <h2 className="text-amber-950 text-4xl pt-4 pl-8 pb-1 font-bold font-[Brave81] tracking-wide text-center">Best Players</h2>
+          <h2 className="text-amber-950 text-4xl pt-4 md:pt-6 lg:pt-7 pl-8 pb-1 font-bold font-[Brave81] tracking-wide text-center">
+            Best Players
+          </h2>
 
           {/**Card Component Traversing */}
           <div className="grid gap-6 grid-cols-2 smd:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-5">
